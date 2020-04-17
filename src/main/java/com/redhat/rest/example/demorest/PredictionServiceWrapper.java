f690a5da86279c00241cd7a5ee545215b85f1a94
Sadhana Nandakumar <snandaku@redhat.com>
2020-04-16 23:40:00 -0400

    added age

--- src/main/java/com/redhat/rest/example/demorest/PredictionServiceWrapper.java
+++ src/main/java/com/redhat/rest/example/demorest/PredictionServiceWrapper.java
@@ -2,36 +2,12 @@ package com.redhat.rest.example.demorest;
 
 
 import org.apache.camel.Exchange;
-import org.apache.camel.Expression;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.builder.xml.XPathBuilder;
-import org.apache.camel.component.http4.HttpComponent;
 import org.apache.camel.model.rest.RestBindingMode;
-import org.apache.camel.util.jsse.KeyStoreParameters;
-import org.apache.camel.util.jsse.SSLContextParameters;
-import org.apache.camel.util.jsse.TrustManagersParameters;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustAllStrategy;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.SSLContexts;
-import org.apache.http.ssl.TrustStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.net.ssl.SSLContext;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 
 @SpringBootApplication
 @RestController
@@ -58,7 +34,7 @@ public class PredictionServiceWrapper {
 				restConfiguration()
 						.component("servlet")
 						.bindingMode(RestBindingMode.auto)
-						.producerComponent("http4").host("localhost:8090");
+						.producerComponent("http4").host("localhost:8080");
 
 
 
